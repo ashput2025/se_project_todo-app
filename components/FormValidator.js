7b83ef6f290df064fb6f549e9227cef05cf2e255
ashput2025 <ashleyput1228@gmail.com>
2025-03-19 01:41:08 -0500

    fix querySelector

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,85 +1,84 @@
 class FormValidator {
-    constructor(settings, formEl) {
-        this._formEl = formEl;
-        this._inputSelector= settings.inputSelector;
-        this._formSelector= settings.formSelector;
-       this._submitButtonSelector= settings.submitButtonSelector;
-       this._errorClass= settings.errorClass;
-       this._inputErrorClass= settings.inputErrorClass;
-       this._inactiveButtonClass= settings.inactiveButtonClass;   
+  constructor(settings, formEl) {
+    this._formEl = formEl;
+    this._inputSelector = settings.inputSelector;
+    this._formSelector = settings.formSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._buttonElement.classList.add(this._inactiveButtonClass);
+      this._buttonElement.disabled = true;
+    } else {
+      this._buttonElement.classList.remove(this._inactiveButtonClass);
+      this._buttonElement.disabled = false;
     }
+  }
 
-    _toggleButtonState(inputList){
-        if (this._hasInvalidInput(this._inputList)){
-            this._buttonElement.classList.add(this._inactiveButtonClass);
-            this._buttonElement.disabled = true;
-        } else {
-            this._buttonElement.classList.remove(this._inactiveButtonClass);
-            this._buttonElement.disabled = false;
-        }
+  _hasInvalidInput = () => {
+    return this._inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  };
+
+  _checkInputValidity(inputElement) {
+    if (!inputElement.validity.valid) {
+      this._showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      this._hideInputError(inputElement);
     }
+  }
 
-    _hasInvalidInput = () => {
-        return this._inputList.some((inputElement) => {
-          return !inputElement.validity.valid;
-        });
-      };
+  _showInputError = (inputElement, errorMessage) => {
+    this._errorElementId = `#${inputElement.id}-error`;
+    this._errorElement = this._formEl.querySelector(this._errorElementId);
+    inputElement.classList.add(this._inputErrorClass);
+    this._errorElement.textContent = errorMessage;
+    this._errorElement.classList.add(this._errorClass);
+  };
 
-      _checkInputValidity(inputElement){
-        if (!inputElement.validity.valid) {
-            this._showInputError(
-              inputElement,
-              inputElement.validationMessage,
-            );
-          } else {
-            this._hideInputError(inputElement );
-          }
-      };
+  _hideInputError = (inputElement) => {
+    this._errorElementId = `#${inputElement.id}-error`;
+    this._errorElement = this._formEl.querySelector(this._errorElementId);
+    inputElement.classList.remove(this._inputErrorClass);
+    this._errorElement.classList.remove(this._errorClass);
+    this._errorElement.textContent = "";
+  };
 
-      _showInputError = (inputElement, errorMessage) => {
-        this._errorElementId = `#${inputElement.id}-error`;
-        this._errorElement = this._formEl.querySelector(this._errorElementId);
-        inputElement.classList.add(this._inputErrorClass);
-        this._errorElement.textContent = errorMessage;
-        this._errorElement.classList.add(this._errorClass);
-      };
+  resetValidation = () => {
+    this._inputList.forEach((inputElement) => {
+      this._hideInputError(inputElement);
+    });
+  };
 
-      _hideInputError = (inputElement) => {
-        this._errorElementId = `#${inputElement.id}-error`;
-        this._errorElement = this._formEl.querySelector(this._errorElementId);
-        inputElement.classList.remove(this._inputErrorClass);
-        this._errorElement.classList.remove(this._errorClass);
-        this._errorElement.textContent = "";
-      };
+  _setEventListeners() {
+    this._inputList = Array.from(
+      this._formEl.querySelectorAll(this._inputSelector)
+    );
+    this._buttonElement = this._formEl.querySelector(
+      this._submitButtonSelector
+    );
 
-      resetValidation = () => {
-        this._inputList.forEach((inputElement) => {
-          this._hideInputError(inputElement);
-        });
-      };
+    this._toggleButtonState();
 
-    _setEventListeners(){
-        this._inputList = Array.from
-        (this._formEl.querySelectorAll(this._inputSelector));
-        this._buttonElement = this._formEl.querySelector(
-            this._submitButtonSelector);
+    this._inputList.forEach(() => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
 
-        this._toggleButtonState(this._inputList);
-
-        this._inputList.forEach((inputElement) => {
-            inputElement.addEventListener("input", () => {
-                this._checkInputValidity(inputElement);
-                this._toggleButtonState(this._inputList);
-            });
-        });
-    }
-
-    enableValidation(){
-        this._formEl.addEventListener("submit", (evt) => {
-            evt.preventDefault();
-        });
-        this._setEventListeners();
-    };
+  enableValidation() {
+    this._formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    this._setEventListeners();
+  }
 }
 
 export default FormValidator;
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ console.log(validationConfig);
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
-const addTodoForm = addTodoPopup.querySelector(".popup__form");
+const addTodoForm = document.forms["add-todo-form"];
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 // const todoTemplate = document.querySelector("#todo-template"); => remove
 const todosList = document.querySelector(".todos__list");
