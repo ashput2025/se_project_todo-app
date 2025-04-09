f902db66de89b558ea9caee2f89288b318ed490c
ashput2025 <ashleyput1228@gmail.com>
2025-04-08 23:55:14 -0500

    add Section class

---
+++ utils/Section.js
@@ -0,0 +1,12 @@
+class Section {
+    constructor({
+        items: ,
+        renderer
+    }, containerSelector){
+        console.log(items);
+        console.log(renderer);
+        console.log(containerSelector);
+    }
+}
+
+export default Section;
