f95285ebd7d19470d13240676f58cb8cfedd8fca
ashput2025 <ashleyput1228@gmail.com>
2025-04-27 20:40:41 -0500

    fix section.js imports

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todos from "../components/Todo.js";
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import FormValidator from "../components/FormValidator.js";
-import Section from "../utils/Section.js";
+import Section from "../components/Section.js";
 import Popup from "../components/Popup.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import TodoCounter from "../components/TodoCounter.js";
