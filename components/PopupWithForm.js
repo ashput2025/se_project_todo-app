ea7afb10ac497945e2df64c1a8adbd965520c0f1
AshleyPutman <ashleyput1228@gmail.com>
2025-05-01 00:53:01 -0500

    relocate date values to inputValues func

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,28 +1,31 @@
 import Popup from "./Popup.js";
 
-class PopupWithForm extends Popup {   
-    constructor ({popupSelector, handleFormSubmit}){
-        super (popupSelector);
-        this._popupForm = this._popupElement.querySelector('.popup__form');
-        this._handleFormSubmit = handleFormSubmit;
-        this._inputList = this._popupForm.querySelectorAll('.popup__input');
-    }
+class PopupWithForm extends Popup {
+  constructor({ popupSelector, handleFormSubmit }) {
+    super(popupSelector);
+    this._popupForm = this._popupElement.querySelector(".popup__form");
+    this._handleFormSubmit = handleFormSubmit;
+    this._inputList = this._popupForm.querySelectorAll(".popup__input");
+  }
 
-    setEventListeners(){
-        super.setEventListeners();
-        this._popupForm.addEventListener('submit', (evt) =>{
-            evt.preventDefault();
-            const inputValues = this._getInputValues(); 
-            this._handleFormSubmit(inputValues);
-        });
+  setEventListeners() {
+    super.setEventListeners();
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      const inputValues = this._getInputValues();
+      this._handleFormSubmit(inputValues);
+    });
+  }
+
+  _getInputValues() {
+    const values = {};
+    this._inputList.forEach((input) => {
+      values[input.name] = input.value;
+    });
+
+    values.adjustedDate = new Date().toISOString();
+    return values;
+  }
 }
 
-    _getInputValues(){
-        const values = {};
-        this._inputList.forEach((input) => {
-                values[input.name] = input.value;
-        });
-        return values;
-    }}
-
-    export default PopupWithForm;
+export default PopupWithForm;
